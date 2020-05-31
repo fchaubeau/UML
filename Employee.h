834b9930ac672202a3ac12a48640cf70f57a6ac4
fchaubeau <f.chaubeau@gmail.com>
2020-05-31 12:10:15 +0200

    a

--- Employee.h
+++ Employee.h
@@ -23,7 +23,7 @@ public:
 
     list<Sensor> getSimilarSensor(const Sensor & s) const;
     vector<double> getMeanAirQuality(const pair<double, double> & center, const double & radius, const time_t & t, const DataManager & dataManager) const;
-    int analyseImpactNiveau(const Cleaning & cleaning, const double & radiusAnalyse, const double & rateAmeliorationP1, const double & rateAmeliorationP2, const DataManager dataManager);
+    int analyseImpactNiveau(const Cleaning & cleaning, const double & radiusAnalyse, const double & rateAmeliorationP1, const double & rateAmeliorationP2, const DataManager & dataManager);
     double getMeanAirQualityTimeSpawn(const pair<double, double> & center, const double & radius, const time_t & tdebut, const time_t & tFin) const;
     bool Menu();
     string toString() const;
