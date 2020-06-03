b339f8faaebec11e3f5c8cd4817272a3715841f3
fchaubeau <f.chaubeau@gmail.com>
2020-06-03 19:25:28 +0200

    commit

--- Employee.h
+++ Employee.h
@@ -28,6 +28,7 @@ public:
     int analyseImpactNiveau(const Cleaning & cleaning, const double & radiusAnalyse, const double & rateAmeliorationP1, const double & rateAmeliorationP2, const DataManager & dataManager);
     void analyseImpactEvolutionDeTemps(const Cleaning & cleaning, const double & radiusAnalyse, const double & rateAmeliorationP1, const double & rateAmeliorationP2, const DataManager & dataManager);
     vector<double> getMeanAirQualityTimeSpawn(const pair<double, double> & center, const double & radius, const time_t & tdebut, const time_t & tFin, const DataManager & dataManager) const;
+    double calculRayonEffet(const Cleaning & cleaning, const double & rateAmeliorationP1, const double & rateAmeliorationP2, const DataManager & dataManager);
     bool Menu();
     string toString() const;
 
